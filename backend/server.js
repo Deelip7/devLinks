b7c6b3bd2d75bfb7faeded5050f109d22ba9e853
Theelipan prabakar <theelipan@gmail.com>
2021-03-14 23:37:32 -0400

    feat: connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 //Entry Point for the server for the back-end
 import express from 'express';
 import linkRoutes from './routers/linkRoutes.js';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
+//loads environment variables from .env
+dotenv.config();
+//Connect to mongoDB
+connectDB();
+
+//app object by calling express()
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//built-in middleware to recognize the incoming Request Object as a JSON Object
 app.use(express.json());
 
 app.use('/api/link', linkRoutes);
 
-app.listen(4000, () => {
-  console.log(`Server run on port 4000`);
-});
+app.listen(PORT, console.log(`Server run on port ${PORT}`));
